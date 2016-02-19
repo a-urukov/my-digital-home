e1e64df07cd5416936d2763ee6df954bbc0c6448
a-urukov <a-urukov@yandex-team.ru>
2016-02-20 01:47:05 +0300

    Верстка блоков категорий

--- script.js
+++ script.js
@@ -48,4 +48,51 @@ $(function() {
 
     current.parents('.b-left-menu__item').addClass('b-left-menu__item_expanded_yes');
     current.addClass('b-left-menu__item_expanded_yes');
+
+    //(function() {
+    //    var HALF_STEP = 20;
+    //
+    //    $('.b-category__image')
+    //        .bind('mouseenter', function(e) {
+    //            var category = $(e.target),
+    //                width = category.width(),
+    //                height = category.height(),
+    //                ratio = width / height;
+    //
+    //            category.css('max-width', width + 'px');
+    //
+    //            if (category.queue().length) {
+    //                category.stop();
+    //            } else {
+    //                category
+    //                    .data('width', width)
+    //                    .data('height', height);
+    //            }
+    //
+    //            category.animate({
+    //                left: '-=' + Math.round(ratio * HALF_STEP),
+    //                top: '-=' + HALF_STEP,
+    //                height: '+=' + HALF_STEP * 2,
+    //                'max-width': '+=' + Math.round(ratio * HALF_STEP * 2),
+    //                'margin-bottom': '-=' + HALF_STEP * 2
+    //            }, 400);
+    //        })
+    //        .bind('mouseleave', function(e) {
+    //            var category = $(e.target);
+    //
+    //            if (category.queue().length) {
+    //                category.stop();
+    //            }
+    //
+    //            category.data('height') && category.data('width') &&
+    //                category.animate({
+    //                    left: '0',
+    //                    top: '0',
+    //                    height: category.data('height'),
+    //                    'max-width': category.data('width'),
+    //                    'margin-bottom': '0'
+    //                }, 400);
+    //        });
+    //})();
+
 });
